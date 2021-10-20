34df454ebfb19bbf94628c8185b857166e7454bc
ddemeter56 <demeter.dobos@gmail.com>
2021-10-20 23:08:33 +0200

    MetricsContainer beautifing

--- src/components/commons/admin/MetricsContainer.js
+++ src/components/commons/admin/MetricsContainer.js
@@ -1,38 +1,82 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { makeStyles } from '@mui/styles';
 import AdminBoxMetrics from './AdminBoxMetrics';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
-import { Typography } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
+import MobileStepper from '@mui/material/MobileStepper';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    padding: 30,
+    width: 300,
+    padding: "15px 0px 0px 15px",
     position: "relative"
   }
 }));
 const MetricsContainer = ({ data, title, forwardButtonText }) => {
+  const theme = useTheme();
   const classes = useStyles();
+  const [activeStep, setActiveStep] = useState(0);
+
+  
+  const changeStepperValue = useCallback(() => {
+    activeStep === data.length - 1 ? setActiveStep(0) : setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  }, [activeStep, data.length]);
+
+  useEffect(() => {
+    const intervalID = setInterval(changeStepperValue, 5000);
+    return () => clearInterval(intervalID);
+  }, [changeStepperValue])
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
   return (
     <Paper className={classes.paper}>
       <Typography variant="h5">{title}</Typography>
       <Grid container alignItems="strech" justifyContent="space-around" spacing={2}>
-        {data.map((item) => {
-          return (
-            <Grid item xl={4} lg={4} sm={4} xs={12} key={item.key}>
-              <div className={classes.paper}>
-                <AdminBoxMetrics title={item.title} numbers={item.numbers} description={item.description} />
-              </div>
-            </Grid>
-          )
-        })}
-      </Grid>
-      <div style={{ position: "absolute", right: 0, bottom: 0, paddingTop: 15}}>
-        <Button variant="text">
-          {forwardButtonText}
-        </Button>
-      </div>
+        <Grid item>
+          <div className={classes.paper}>
+            <AdminBoxMetrics title={data[activeStep].title} numbers={data[activeStep].numbers} description={data[activeStep].description} />
+          </div>
+        </Grid>
+      </Grid> 
+      <MobileStepper
+        variant="dots"
+        steps={data.length}
+        position="relative"
+        activeStep={activeStep}
+        sx={{ maxWidth: 400, flexGrow: 1 }}
+        nextButton={
+          <Button size="small" onClick={handleNext} disabled={activeStep === data.length -1}>
+          Next
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+          Back
+          </Button>
+        }
+      />
     </Paper>
   )
 }
