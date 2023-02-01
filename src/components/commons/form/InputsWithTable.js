f21cd84078a8c9df7ce4b684bc17e2cff796d43e
DEMELAPTOP\demet <demeter.dobos@gmail.com>
2023-02-01 23:11:13 +0100

    working selection in table, work has to be done with it, still

--- src/components/commons/form/InputsWithTable.js
+++ src/components/commons/form/InputsWithTable.js
@@ -11,6 +11,7 @@ import TextField from "@mui/material/TextField";
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Context from '../../../context/register.context';
+import EnhancedTable from '../enhanced-table/EnhancedTable';
 const useStyles = makeStyles((theme) => ({
   textField: {
     [theme.breakpoints.up('lg')]: {
@@ -39,7 +40,7 @@ const InputsWithTable = ({ formData, stateIdentifier }) => {
 
   const formDataNames = formData.map(item => item.name)
 
-  console.log(formData)
+  
   console.log(inputCollection)
   function createInitCollection() {
     let objectWithFormKeys = {}
@@ -69,24 +70,6 @@ const InputsWithTable = ({ formData, stateIdentifier }) => {
     console.log(dataRow)
   }
 
-  function createData(
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-  ) {
-    return { name, calories, fat, carbs, protein };
-  }
-
-  const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
-
   return (
     <>
       <Grid container spacing={4}>
@@ -110,6 +93,7 @@ const InputsWithTable = ({ formData, stateIdentifier }) => {
           Add to table
         </Button>
       </Grid>
+      <EnhancedTable formData={formData} rows={dataRow}/>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
